ab805ac1a7832cc6c84541a17b995acbfbd2cbaf
liuxiaochen0625 <liuxiaoqiang_0625@163.com>
2016-03-08 14:12:37 +0800

    Spring boot 与 JSP

---
+++ src/main/java/com/reus/lxq/SampleWebJspApplication.java
@@ -0,0 +1,28 @@
+package com.reus.lxq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Hello world!
+ *
+ */
+@Configuration
+@EnableAutoConfiguration
+@ComponentScan
+public class SampleWebJspApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SampleWebJspApplication.class);
+	}
+
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(SampleWebJspApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/reus/lxq/WelcomeController.java
@@ -0,0 +1,33 @@
+package com.reus.lxq;
+
+import java.util.Date;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+
+	@Value("${application.message:Hello World}")
+	private String message = "Hello World";
+
+	@RequestMapping("/action")
+	public String welcome(Map<String, Object> model) {
+		model.put("time", new Date());
+		model.put("message", this.message);
+		return "welcome";
+	}
+
+	@RequestMapping("/foo")
+	public String foo(Map<String, Object> model) {
+		throw new RuntimeException("Foo");
+	}
+	@RequestMapping("/hello")
+	public String sayHello(){
+		return "Hello world"; 
+	}
+
+}
